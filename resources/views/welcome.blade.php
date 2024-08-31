a82a4dbb4b85c8a89c6637300dfe807c9d9e9e9f
Liban Hassan <liban64@gmail.com>
2024-09-01 02:23:49 +0900

    Include .vite directory

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Homework App</title>
+    @vite('resources/js/app.js') <!-- Ensure Vite loads the compiled app.js -->
 </head>
 <body>
     <div id="app"></div> <!-- Vue app will be mounted here -->
