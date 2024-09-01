f9a971091e6fcbecc9d729dabb7c32979f7f4d50
Liban Hassan <liban64@gmail.com>
2024-09-01 11:35:06 +0900

    Include Vite build assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Homework App</title>
-    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
-    <script src="{{ asset('build/assets/app.js') }}" defer></script>
+    @vite('resources/js/app.js')
+
 
 </head>
 <body>
